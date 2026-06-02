5322ba21732f7aaa000d3051db5da4844ea48163
Euan Munro <euanmunroo@gmail.com>
2026-06-02 15:40:26 +0100

    making changes to full site

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,11 @@ import HeroSection from "@/components/home/HeroSection";
 import ServiceCards from "@/components/home/ServiceCards";
 import StatsSection from "@/components/home/StatsSection";
 import LocationsSection from "@/components/home/LocationsSection";
-import TestimonialsSection from "@/components/home/TestimonialsSection";
 import GoogleReviewsSection from "@/components/home/GoogleReviewsSection";
 import CTASection from "@/components/home/CTASection";
 import WhyChooseSection from "@/components/home/WhyChooseSection";
 import UpcomingCoursesSection from "@/components/home/UpcomingCoursesSection";
-import { getTestimonials } from "@/lib/storage";
-
 export default async function HomePage() {
-  const dbTestimonials = await getTestimonials(true);
-  const featured = dbTestimonials.filter((t) => t.featured).slice(0, 4);
-  const testimonialsForHome =
-    featured.length > 0
-      ? featured.map((t) => ({
-          name: t.name,
-          company: t.company,
-          role: t.role,
-          text: t.text,
-          rating: t.rating,
-          initial: t.name.charAt(0).toUpperCase(),
-        }))
-      : undefined;
-
   return (
     <>
       <HeroSection />
@@ -31,7 +14,6 @@ export default async function HomePage() {
       <UpcomingCoursesSection />
       <StatsSection />
       <WhyChooseSection />
-      <TestimonialsSection testimonials={testimonialsForHome} />
       <GoogleReviewsSection />
       <LocationsSection />
       <CTASection />
